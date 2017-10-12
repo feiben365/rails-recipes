9094178f2d3907392c1c2dff3714e5c4b2346e3a
feiben <feiben365@gmail.com>
2017-10-12 17:02:27 +0800

    user can change timezone

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :set_timezone
+
+  def set_timezone
+    if current_user && current_user.time_zone
+      Time.zone = current_user.time_zone
+    end
+  end
 end
