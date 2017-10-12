bd52ed7c809c9e3cfe8bd8e5bc9365e2ee64329e
feiben <feiben365@gmail.com>
2017-10-12 15:09:11 +0800

    single resouce profile frontend and backend added

--- db/schema.rb
+++ db/schema.rb
@@ -45,8 +45,17 @@ ActiveRecord::Schema.define(version: 20171012014729) do
   end
 
   create_table "profiles", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "legal_name"
+    t.date     "birthday"
+    t.string   "location"
+    t.string   "education"
+    t.string   "occupation"
+    t.text     "bio"
+    t.text     "specialty"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
